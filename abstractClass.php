55ee4ce31176796ccaefa40f2ef284284929e91f
WinterCricket <hodhodhod2003@gmail.com>
2017-06-01 17:38:25 -0500

    add some cars

--- abstractClass.php
+++ abstractClass.php
@@ -5,12 +5,12 @@ abstract class Car{
 	public $color;
 	public $price;
 
-	public function report($name, $color, $price){
-		return $this->name." is ".$this->color." and will cost ".$this->price." dollars.";
+	public function report(){
+		return "This ".$this->name." is ".$this->color." and will cost ".$this->price." dollars. <br />";
 	}
 	abstract public function showCar();
 }
-class Anvil extends Car{
+class Porche extends Car{
 	public function report(){
 		return parent::report();
 	}
@@ -18,5 +18,27 @@ class Anvil extends Car{
 		echo "Behold this ".$this->name."!";
 	}
 }
+class Toyota extends Car{
+	public function report(){
+		return parent::report();
+	}
+	public function showCar(){
+		echo "Behold this ".$this->name."!";
+	}
+}
+
+ $car = new Porche();
+ $car->name = "Condini";
+ $car->color = "Forest Green";
+ $car->price = "54,000";
+ echo $car -> showCar();
+ echo $car -> report();
+
+
+ $car = new Toyota();
+ $car->name = "Camry";
+ $car->color = "Silver";
+ $car->price = "8,000";
+ echo $car -> report();
 
 ?>
