bfa9a2e39132b65b70192c50d1da5726522cd6cf
WinterCricket <hodhodhod2003@gmail.com>
2017-06-01 21:09:11 -0500

    use extends for class inherit

--- air.php
+++ air.php
@@ -1,8 +1,8 @@
 <?php 
 
-	class Air{
-		public function discourageStudents(){
-			echo "Life is meaningless, especially as those you love, your allies in this life, begin to die off. <br />";
+	class Air extends land{
+		public function greetStudents(){
+			echo "Message from those who no longer behold you with love. <br />";
 		}
 	}
 
