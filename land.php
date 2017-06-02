0f209432f79f98acdafa436b0acfb94116db0d93
WinterCricket <hodhodhod2003@gmail.com>
2017-06-01 21:14:53 -0500

    use final key to make class final and not written over by inherit

--- land.php
+++ land.php
@@ -1,7 +1,7 @@
 <?php 
 
-	class Land{
-		final public function greetStudents(){
+	final class Land{
+		public function greetStudents(){
 			echo "Plan your book the same way you'd write it: a bit at a time. <br />";
 		}
 	}
